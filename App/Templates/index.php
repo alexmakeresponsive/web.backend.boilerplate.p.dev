6d53888c878e32eee1c5e7fca9fccbaf40d4fabd
Alexandr Gorchakov <alexmakeresponsive@mail.ru>
2018-09-06 17:32:00 +0600

    render data via template

---
+++ App/Templates/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Новости</title>
+    <style>
+        body {
+            font-family: sans-serif;
+            font-size: 14px;
+        }
+    </style>
+</head>
+<body>
+
+    <h1>Новости</h1>
+
+    <?php foreach ( $articles as $article ) { ?>
+        <article>
+            <h3><?php echo $article->title ?></h3>
+            <p><?php echo $article->content ?></p>
+        </article>
+        <hr>
+    <?php } ?>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -9,15 +9,18 @@ require __DIR__ . '/autoload.php';
 //$article = new App\Models\Article();
 //$result = $article->findAll();
 
-$news = \App\Models\Article::findAll();
-$users = \App\Models\User::findAll();
+//$news = \App\Models\Article::findAll();
+//$users = \App\Models\User::findAll();
 
-$article = new \App\Models\Article();
-$article->title = 'Hyundai может выпустить конкурента Toyota Land Cruiser 200';
-$article->content = 'Новая модель может появиться в линейке Hyundai минимум через три года, сообщает австралийское издание Car Sales. По информации портала, это будет полноразмерный внедорожник, главным конкурентом которого станет Toyota Land Cruiser 200. Если проект получит «зеленый свет», то основными рынками для него будут, собственно, Австралия, Ближний Восток и, возможно, США. Впрочем, в России тоже любят большие машины, так что и в нашей стране такая модель Hyundai наверняка нашла бы покупателей.';
+//$article = new \App\Models\Article();
+//$article->title = 'Hyundai может выпустить конкурента Toyota Land Cruiser 200';
+//$article->content = 'Новая модель может появиться в линейке Hyundai минимум через три года, сообщает австралийское издание Car Sales. По информации портала, это будет полноразмерный внедорожник, главным конкурентом которого станет Toyota Land Cruiser 200. Если проект получит «зеленый свет», то основными рынками для него будут, собственно, Австралия, Ближний Восток и, возможно, США. Впрочем, в России тоже любят большие машины, так что и в нашей стране такая модель Hyundai наверняка нашла бы покупателей.';
+//
+//$article->insertOne();
 
-$article->insertOne();
+//echo '<pre>';
+//var_dump($news);
+//echo '</pre>';
 
-echo '<pre>';
-var_dump($news);
-echo '</pre>';
+$articles = \App\Models\Article::findAll();
+include __DIR__ . '/App/Templates/index.php';
